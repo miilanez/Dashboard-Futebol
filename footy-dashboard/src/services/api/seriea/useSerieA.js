2e5b065fd5bff8d196ee364da13bdd9e6497d588
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2023-02-24 10:14:49 -0300

    feat: configuração das apis de cada liga

---
+++ footy-dashboard/src/services/api/seriea/useSerieA.js
@@ -0,0 +1,70 @@
+import { useCallback } from "react";
+import api from "../api";
+const path = "seriea";
+
+function useSerieA() {
+  const getStandings = useCallback(async () => {
+    try {
+      const options = await api.get(`${path}/table`);
+      return options.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  const getNews = useCallback(async () => {
+    try {
+      const options = await api.get(`${path}/news`);
+      return options.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  const getResults = useCallback(async () => {
+    try {
+      const options = await api.get(`${path}/results`);
+      return options.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  const getFixtures = useCallback(async () => {
+    try {
+      const options = await api.get(`${path}/fixtures`);
+      return options.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  const getSquadname = useCallback(async (team) => {
+    try {
+      const options = await api.get(`${path}/table/squadname/${team}`);
+      return options.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  const getTransfers = useCallback(async () => {
+    try {
+      const options = await api.get(`${path}/transfers`);
+      return options.data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
+  return {
+    getStandings,
+    getNews,
+    getTransfers,
+    getSquadname,
+    getFixtures,
+    getResults,
+  };
+}
+
+export default useSerieA;
