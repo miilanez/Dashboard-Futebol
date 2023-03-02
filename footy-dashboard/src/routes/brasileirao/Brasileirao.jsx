497c9161f32e73406fbf1c09e122146169f917e1
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2023-03-02 18:20:01 -0300

    feat: ajustes nas paginas ligue1 e brasileirão

--- footy-dashboard/src/routes/brasileirao/Brasileirao.jsx
+++ footy-dashboard/src/routes/brasileirao/Brasileirao.jsx
@@ -1,13 +1,230 @@
-import React from 'react'
+import "./style.css";
 import "../mainStyles.css";
-import "./styles.css"
+
+import { useState, useEffect } from "react";
+
+import BrasileiraoLogo from "../../assets/images/logos/brasileirao.png";
+
+//imports components
+import Standings from "../../components/standings/Standings";
+import PageTitle from "../../components/pageTitle/PageTitle";
+import MainNotice from "../../components/news/MainNotice";
+import News from "../../components/news/News";
+import Matches from "../../components/macthes/Matches";
+
+//imports api
+import useBrasileirao from "../../services/api/brasileirao/useBrasileirao";
+import Loading from "../../patterns/loading/Loading";
 
 const Brasileirao = () => {
-  return (
-    <div>
-      Brasileirão
-    </div>
-  )
-}
+  //Tempo de Requisição
+  const timeRefresh = 6000000;
 
-export default Brasileirao
+  //loading
+  const [loading, setLoading] = useState(false);
+
+  //Request Standings
+
+  const [standing, setStanding] = useState([]);
+  const { getStandings } = useBrasileirao();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getStandings();
+        setStanding(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getStandings]);
+
+  //Request Transfers
+
+  // const [transfers, setTransfer] = useState([]);
+  // const { getTransfers } = useBrasileirao();
+
+  // useEffect(() => {
+  //   async function fetch() {
+  //     try {
+  //       const dados = await getTransfers();
+  //       setTransfer(dados);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   }
+  //   function firstRequest() {
+  //     fetch();
+  //   }
+
+  //   if (firstRequest) {
+  //     firstRequest = false;
+  //     fetch();
+  //   }
+  // }, [getTransfers]);
+
+  // console.log("tranferencias", transfers)
+
+  //Request News
+
+  const [news, setNews] = useState([]);
+  const { getNews } = useBrasileirao();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getNews();
+        setNews(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getNews]);
+
+  //Request Squadname
+  // const [squadname, setSquadname] = useState([]);
+  // const { getSquadname } = useBrasileirao();
+
+  // useEffect(() => {
+  //   async function fetch() {
+  //     try {
+  //       const dados = await getSquadname();
+  //       setSquadname(dados);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   }
+  //   function firstRequest() {
+  //     fetch();
+  //   }
+
+  //   if (firstRequest) {
+  //     firstRequest = false;
+  //     fetch();
+  //   }
+  // }, [getSquadname]);
+
+  //Request Fixtures
+
+  const [fixtures, setFixtures] = useState([]);
+  const { getFixtures } = useBrasileirao();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getFixtures();
+        setFixtures(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getFixtures]);
+
+  // console.log("fixtures", fixtures)
+
+  //Request Results
+
+  const [results, setResults] = useState([]);
+  const { getResults } = useBrasileirao();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getResults();
+        setResults(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getResults]);
+
+  // console.log("resultados", results);
+
+  return (
+    <div className="page brasileirao">
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="main-container">
+          <div className="upper-container">
+            <div>
+              <PageTitle
+                title="Brasileirão"
+                logoLeague={BrasileiraoLogo}
+              />
+            </div>
+            <div>
+              <MainNotice data={news} />
+            </div>
+            <div>
+              <News data={news} />
+            </div>
+          </div>
+
+          <div className="downner-container">
+            <Matches results={results} fixtures={fixtures} />
+            <Standings data={standing} title="Brasileirão" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Brasileirao;
--- footy-dashboard/src/routes/ligue1/Ligue1.jsx
+++ footy-dashboard/src/routes/ligue1/Ligue1.jsx
@@ -1,9 +1,227 @@
-import React from "react";
 import "./style.css";
 import "../mainStyles.css";
 
+import { useState, useEffect } from "react";
+
+import Ligue1Logo from "../../assets/images/logos/ligue1.png";
+
+//imports components
+import Standings from "../../components/standings/Standings";
+import PageTitle from "../../components/pageTitle/PageTitle";
+import MainNotice from "../../components/news/MainNotice";
+import News from "../../components/news/News";
+import Matches from "../../components/macthes/Matches";
+
+//imports api
+import useLigue1 from "../../services/api/ligue1/useLigue1";
+import Loading from "../../patterns/loading/Loading";
+
 const Ligue1 = () => {
-  return <div>Ligue1</div>;
+  //Tempo de Requisição
+  const timeRefresh = 6000000;
+
+  //loading
+  const [loading, setLoading] = useState(false);
+
+  //Request Standings
+
+  const [standing, setStanding] = useState([]);
+  const { getStandings } = useLigue1();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getStandings();
+        setStanding(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getStandings]);
+
+  //Request Transfers
+
+  // const [transfers, setTransfer] = useState([]);
+  // const { getTransfers } = useLigue1();
+
+  // useEffect(() => {
+  //   async function fetch() {
+  //     try {
+  //       const dados = await getTransfers();
+  //       setTransfer(dados);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   }
+  //   function firstRequest() {
+  //     fetch();
+  //   }
+
+  //   if (firstRequest) {
+  //     firstRequest = false;
+  //     fetch();
+  //   }
+  // }, [getTransfers]);
+
+  // console.log("tranferencias", transfers)
+
+  //Request News
+
+  const [news, setNews] = useState([]);
+  const { getNews } = useLigue1();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getNews();
+        setNews(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getNews]);
+
+  //Request Squadname
+  // const [squadname, setSquadname] = useState([]);
+  // const { getSquadname } = useLigue1();
+
+  // useEffect(() => {
+  //   async function fetch() {
+  //     try {
+  //       const dados = await getSquadname();
+  //       setSquadname(dados);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   }
+  //   function firstRequest() {
+  //     fetch();
+  //   }
+
+  //   if (firstRequest) {
+  //     firstRequest = false;
+  //     fetch();
+  //   }
+  // }, [getSquadname]);
+
+  //Request Fixtures
+
+  const [fixtures, setFixtures] = useState([]);
+  const { getFixtures } = useLigue1();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getFixtures();
+        setFixtures(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getFixtures]);
+
+  // console.log("fixtures", fixtures)
+
+  //Request Results
+
+  const [results, setResults] = useState([]);
+  const { getResults } = useLigue1();
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      try {
+        const dados = await getResults();
+        setResults(dados);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    }
+    function firstRequest() {
+      fetch();
+    }
+
+    if (firstRequest) {
+      firstRequest = false;
+      fetch();
+    }
+
+    const interval = setInterval(fetch, timeRefresh); //60s
+    return () => clearInterval(interval);
+  }, [getResults]);
+
+  // console.log("resultados", results);
+
+  return (
+    <div className="page ligueone">
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="main-container">
+          <div className="upper-container">
+            <div>
+              <PageTitle title="Ligue 1" logoLeague={Ligue1Logo} />
+            </div>
+            <div>
+              <MainNotice data={news} />
+            </div>
+            <div>
+              <News data={news} />
+            </div>
+          </div>
+
+          <div className="downner-container">
+            <Matches results={results} fixtures={fixtures} />
+            <Standings data={standing} title="Ligue 1" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Ligue1;
