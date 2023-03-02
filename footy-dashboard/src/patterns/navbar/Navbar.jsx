88cb3fd7e9bd642e1a626c01cc5951a2fc0bef8c
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2023-03-02 17:21:47 -0300

    feat: criação da pag. home

--- footy-dashboard/src/patterns/navbar/Navbar.jsx
+++ footy-dashboard/src/patterns/navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import "./navbar.css";
 
 import { Link } from "react-router-dom";
 
+import logoDashboard from "../../assets/images/logos/logoDashboard.png";
+
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState("home");
 
@@ -10,80 +12,77 @@ const Navbar = () => {
     setActiveLink(link);
   };
   return (
-    <div >
+    <div>
       <nav id="navbar">
-        <ul>
-          <li>
-            <Link
-              to="/"
-              exact
-              onClick={() => handleLinkClick("home")}
-              className={activeLink === "home" ? "active" : ""}
-            >
-              Principal
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/premierleague"
-              exact
-              onClick={() => handleLinkClick("premierleague")}
-              className={activeLink === "premierleague" ? "active" : ""}
-            >
-              Premier League
-            </Link>
-          </li>
-          <li>
-          <Link
-              to="/seriea"
-              exact
-              onClick={() => handleLinkClick("seriea")}
-              className={activeLink === "seriea" ? "active" : ""}
-            >
-              Serie A
-            </Link>
-          </li>
-          <li>
-          <Link
-              to="/laliga"
-              exact
-              onClick={() => handleLinkClick("laliga")}
-              className={activeLink === "laliga" ? "active" : ""}
-            >
-              La Liga
-            </Link>
-          </li>
-          <li>
-          <Link
-              to="/bundesliga"
-              exact
-              onClick={() => handleLinkClick("bundesliga")}
-              className={activeLink === "bundesliga" ? "active" : ""}
-            >
-              Bundesliga
-            </Link>
-          </li>
-          <li>
-          <Link
-              to="/ligue1"
-              exact
-              onClick={() => handleLinkClick("ligue1")}
-              className={activeLink === "ligue1" ? "active" : ""}
-            >
-              Ligue 1
-            </Link>
-          </li>
-          <li>
-          <Link
-              to="/brasileirao"
-              exact
-              onClick={() => handleLinkClick("brasileirao")}
-              className={activeLink === "brasileirao" ? "active" : ""}
-            >
-              Brasileirão
-            </Link>
-          </li>
-        </ul>
+        <div className="logo">
+          <Link to="/" exact>
+            <img src={logoDashboard} />
+          </Link>
+        </div>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <Link
+                to="/premierleague"
+                exact
+                onClick={() => handleLinkClick("premierleague")}
+                className={activeLink === "premierleague" ? "active" : ""}
+              >
+                Premier League
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/seriea"
+                exact
+                onClick={() => handleLinkClick("seriea")}
+                className={activeLink === "seriea" ? "active" : ""}
+              >
+                Serie A
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/laliga"
+                exact
+                onClick={() => handleLinkClick("laliga")}
+                className={activeLink === "laliga" ? "active" : ""}
+              >
+                La Liga
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/bundesliga"
+                exact
+                onClick={() => handleLinkClick("bundesliga")}
+                className={activeLink === "bundesliga" ? "active" : ""}
+              >
+                Bundesliga
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/ligue1"
+                exact
+                onClick={() => handleLinkClick("ligue1")}
+                className={activeLink === "ligue1" ? "active" : ""}
+              >
+                Ligue 1
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/brasileirao"
+                exact
+                onClick={() => handleLinkClick("brasileirao")}
+                className={activeLink === "brasileirao" ? "active" : ""}
+              >
+                Brasileirão
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       {/* <hr /> */}
     </div>
--- footy-dashboard/src/routes/home/Home.jsx
+++ footy-dashboard/src/routes/home/Home.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import "./style.css";
 
+import { Link } from "react-router-dom";
+
+//image imports
+
+import logoPremier from "../../assets/images/logos/premierLeague.png";
+import logoSerieA from "../../assets/images/logos/serieA.png";
+import logoLaLiga from "../../assets/images/logos/laLiga.png";
+import logoLigue1 from "../../assets/images/logos/ligue1.png";
+import logoBundesliga from "../../assets/images/logos/bundesliga.png";
+import logoBrasileirao from "../../assets/images/logos/brasileirao.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="page home">
+      <div className="main">
+        <div className="content">
+          <div className="content-left"></div>
+          <div className="content-right">
+            <div className="sobre">
+              <h1>Futebol em tempo real</h1>
+              <h3>
+                Esta aplicação fornece dados dos principais campeonatos de
+                futebol do planeta.
+              </h3>
+              <h3>
+                Dados dos torneios, partidas jogadas, próxima rodada, notícias
+                das ligas e mais.
+              </h3>
+              <h4>
+                Todos os dados são retornados da Football API do Giuliano
+                Crescimbeni.
+              </h4>
+
+              <div className="league-logos">
+                <Link to="/premierleague" exact>
+                  <img src={logoPremier} />
+                </Link>
+                <Link to="/seriea" exact>
+                  <img src={logoSerieA} />
+                </Link>
+                <Link to="/laliga" exact>
+                  <img src={logoLaLiga} />
+                </Link>
+                <Link to="/bundesliga" exact>
+                  <img src={logoBundesliga} />
+                </Link>
+                <Link to="/ligue1" exact>
+                  <img src={logoLigue1} />
+                </Link>
+                <Link to="/brasileirao" exact>
+                  <img src={logoBrasileirao} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
